ef7b13d530f923c956e2d73a46a11e27782b41db
Jay Patel <56035052+jppatel5375@users.noreply.github.com>
2023-10-23 15:41:11 +0530

    Update generate.py

--- generate.py
+++ generate.py
@@ -164,4 +164,16 @@ def main():
 
     parser.add_argument('--network', help='Network pickle filename', dest='network_pkl', required=True)
     g = parser.add_mutually_exclusive_group(required=True)
-    g.add_argument('--seeds', type=_parse_num_range,
+    g.add_argument('--seeds', type=_parse_num_range, help='List of random seeds')
+    g.add_argument('--dlatents', dest='dlatents_npz', help='Generate images for saved dlatents')
+    parser.add_argument('--trunc', dest='truncation_psi', type=float, help='Truncation psi (default: %(default)s)', default=0.5)
+    parser.add_argument('--class', dest='class_idx', type=int, help='Class label (default: unconditional)')
+    parser.add_argument('--outdir', help='Where to save the output images', required=True, metavar='DIR')
+
+    args = parser.parse_args()
+    generate_images(**vars(args))
+
+#----------------------------------------------------------------------------
+
+if __name__ == "__main__":
+    main()
